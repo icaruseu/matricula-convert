59845c39d18746731d5d74c7c7906ed65e12d556
Daniel Jeller <djeller@nettek.at>
2024-09-26 10:10:55 +0200

    Add high res scaling settings for QT

--- matricula-convert.py
+++ matricula-convert.py
@@ -1,11 +1,15 @@
 import sys
 
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication
 
 from modules.gui.main_window import MainWindow
 from modules.logger import Logger
 
 log = Logger()
+
+QApplication.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling)
+QApplication.setAttribute(Qt.ApplicationAttribute.AA_UseHighDpiPixmaps)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
